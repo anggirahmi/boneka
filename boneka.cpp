343c325823111ee2600809486c8dd504a5b433fd
anggirahmi <anggirahmi07@gmail.com>
2018-01-19 19:07:26 +0800

    ukuran

--- boneka.cpp
+++ boneka.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class boneka{
 	public:
-			void bahan(string busa)
+			void bahan(string busa);
 			string warna;
 			double ukuran;
 };
@@ -13,11 +13,15 @@ int main(){
 	boneka beruang;
 	
 	beruang.warna ="coklat";
-	beruang.ukuran ="besar";
+	beruang.ukuran =30;
 	
 	cout<<"Warna boneka beruang "<< (Beruang.warna)<<endl;
 	cout<<"Harga sesuai label "<< (Beruang.ukuran)<<endl;
 	
 	Beruang.bahan("Bahan busanya sangat lembut");
+}
+
+void boneka::bahan(string busa){
+	cout<<""
 }
 
